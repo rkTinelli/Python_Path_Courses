2a5925084598959d2335fc36bec6beae4f4a60ba
Henrique <rktinelli@gmail.com>
2020-05-14 20:21:13 +0100

    Part 1 Class 3 Exercises done

--- Part 1 - Intro/guess_number.py
+++ Part 1 - Intro/guess_number.py
@@ -8,11 +8,16 @@ guess_str = input("What is your number? ")
 print("Your guess was: ", guess_str)
 chute = int(guess_str)
 
-if (secret_number == chute):
-    print("You won!")
+won = chute == secret_number
+bigger = chute > secret_number
+smaller = chute < secret_number
+
+if (won):
+    print("You won!!")
 else:
-    print("You lost! :( ")
+    if (bigger):
+        print("You lost! Keep in mind that your guess was bigger than the secret number")
+    elif (smaller):
+        print("You lost! Keep in mind that your guess was smaller than the secret number")
 
 print("Game Over")
-
-print("ola", "mundo")
