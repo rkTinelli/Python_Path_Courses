b4034f36a8d67f36c9466de62f5ef0d1fe71595d
Henrique <rktinelli@gmail.com>
2020-05-14 20:58:06 +0100

    Part 1 Class 5 Exercises done

--- Part 1 - Intro/guess_number.py
+++ Part 1 - Intro/guess_number.py
@@ -3,20 +3,21 @@ print("* Welcome to the Guessing Number Game *")
 print("* * * * * * * * * * * * * * * * * * * *")
 
 secret_number = 42
-
 max_n_tries = 3
-round = 1
 
-while (round <= max_n_tries):
-
-    print("Round", round, "of", max_n_tries)
+for round in range(1, max_n_tries+1):
+    print("Round {} of {}".format(round, max_n_tries))
     guess_str = input("What is your number? ")
     print("Your guess was: ", guess_str)
-    chute = int(guess_str)
+    guess = int(guess_str)
 
-    won = chute == secret_number
-    bigger = chute > secret_number
-    smaller = chute < secret_number
+    if (guess < 1 or guess > 100):
+        print("You must use a number between 1 and 100!")
+        continue
+
+    won = guess == secret_number
+    bigger = guess > secret_number
+    smaller = guess < secret_number
 
     if (won):
         print("You won!!")
