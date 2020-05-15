81495eaddf95f5bfddaa83a5633e72610375b9fd
Henrique <rktinelli@gmail.com>
2020-05-15 11:50:23 +0100

    Part 2 Class 2 Exercises done

--- Part 2/hangman.py
+++ Part 2/hangman.py
@@ -3,14 +3,23 @@ def play():
     print("* Welcome to the Hangman Game *")
     print("* * * * * * * * * * * * * * * *")
 
-    secret_word = "banana"
+    secret_word = ("banana").upper()
 
     hanged = False
     won = False
 
     while not won and not hanged:
-        print("Still playing...")
 
+        user_letter = input(print("What is your guess?: ")).upper()
+        user_letter = user_letter.strip()
+
+        index = 0
+        for letter in secret_word:
+            if letter == user_letter:
+                print("Found {} in the position {}".format(user_letter,str(index)))
+            index = index + 1
+
+        print("Playing...")
 
     print("Game Over")
 
