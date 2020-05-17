e9a9ceb54d9ff0ceb2df849ad8a53345d58a7f19
Henrique <rktinelli@gmail.com>
2020-05-17 13:41:10 +0100

    OOP Part 1 Class 2 Exercises done

---
+++ OOP -Part1/account.py
@@ -0,0 +1,8 @@
+class Account:
+
+    # Constructor function
+    def __init__(self, number, owner, balance, limit):
+        self.number = number
+        self.owner = owner
+        self.balance = balance
+        self.limit = limit
