34527d972490fb22754609fd6f90312fe5d86e92
Henrique <rktinelli@gmail.com>
2020-05-21 19:21:56 +0100

    OOP Part 2 Class 5 Exercises done

--- OOP -Part2/models.py
+++ OOP -Part2/models.py
@@ -41,17 +41,41 @@ class Series(Show):
         return f'Name: {self._name} - {self.seasons} seasons - Likes: {self._likes}'
 
 
+class Playlist():
+    def __init__(self, name, shows):
+        self.nome = name
+        self._shows = shows
+
+    def __getitem__(self, item):
+        return self._shows[item]
+
+    def __len__(self):
+        return len(self._shows)
+
+
 avengers = Movie('Avengers Infinity War', 2018, 160)
+john_wick = Movie('John Wick', 2014, 107)
 atlanta = Series('atlanta', 2018, 2)
+b99 = Series('Brooklyn Nine-Nine ', 2013, 7)
 
 avengers.receive_likes()
 avengers.receive_likes()
 avengers.receive_likes()
+
+john_wick.receive_likes()
+john_wick.receive_likes()
 
 atlanta.receive_likes()
 atlanta.receive_likes()
 
-playlist = [avengers,atlanta]
+b99.receive_likes()
+b99.receive_likes()
+b99.receive_likes()
+b99.receive_likes()
+
+playlist = [avengers,atlanta, john_wick, b99]
+
+print(f'Playlist size: {len(playlist)} items')
 
 for show in playlist:
     print(show)
