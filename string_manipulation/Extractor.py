b1a52c08ea63b8cc92058204e94a6a59c024769a
Henrique <rktinelli@gmail.com>
2020-05-31 14:45:23 +0100

    String Class3 added

--- string_manipulation/Extractor.py
+++ string_manipulation/Extractor.py
@@ -1,16 +1,16 @@
 class ExtractorArgUrl():
     def __init__(self, url):
         if self.valid_url(url):        
-            self.url = url
+            self.url = url.lower()
         else:
             raise LookupError("Invalid URL provided")
 
     @staticmethod
     def valid_url(url):
-        if url == None:
-            return False
-        else:
+        if url and url.startswith("https://bytebank.com"):
             return True
+        else:
+            return False
 
     def get_arguments(self):
 
@@ -21,18 +21,35 @@ class ExtractorArgUrl():
         end_index_orin_currency = self.url.find("&")
         original_currency = self.url[init_index_orin_currency:end_index_orin_currency]
 
+        if original_currency == "moedadestino":
+            original_currency = self.replaceOriginCurrency(tag_orin_currency)
+
         init_index_dest_currency = self.find_start_index_substring(tag_dest_currency)
-        future_currency = self.url[init_index_dest_currency:]
+        end_index_dest_currency = self.url.find("&",init_index_dest_currency)
+        future_currency = self.url[init_index_dest_currency:end_index_dest_currency]
 
         return original_currency, future_currency
 
     def find_start_index_substring(self, currency):
         return self.url.find(currency) + len(currency) + 1
 
+    def replaceOriginCurrency(self, originCurrency):
+        self.url = self.url.replace("moedadestino", "real", 1)  # primeiro vou fazer sem o 1 e depois vou usa-lo
+        init_index_orin_currency = self.find_start_index_substring(originCurrency)
+        end_index_orin_currency = self.url.find("&")
+        return self.url[init_index_orin_currency:end_index_orin_currency]
+
+    def get_value(self):
+        value_tag = "Valor".lower()
+        init_index_valeu = self.find_start_index_substring(value_tag)
+        value = self.url[init_index_valeu:]
+        return value
+
 
 if __name__ == "__main__":
 
-    url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dólar"
+    url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dolar&valor=1500"
     arg = ExtractorArgUrl(url)
     original_currency, future_currency = arg.get_arguments()
-    print(future_currency +"\t"+ original_currency)
+    currency_amount = arg.get_value()
+    print(future_currency +"\t"+ original_currency +"\t"+ currency_amount)
